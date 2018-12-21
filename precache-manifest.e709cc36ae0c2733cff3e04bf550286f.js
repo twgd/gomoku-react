cbb2a1826dab62a49cfbcd259a18d4d46c5ca5fe
twgd <twgd.huang@gmail.com>
2018-12-21 18:47:36 +0800

    Updates

---
+++ precache-manifest.e709cc36ae0c2733cff3e04bf550286f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c458845f7a3feb23084",
+    "url": "/gomoku-react/static/js/runtime~main.9c458845.js"
+  },
+  {
+    "revision": "a042bd7ecea9c7caa607",
+    "url": "/gomoku-react/static/js/main.a042bd7e.chunk.js"
+  },
+  {
+    "revision": "ccde91d822183898ebdf",
+    "url": "/gomoku-react/static/js/1.ccde91d8.chunk.js"
+  },
+  {
+    "revision": "a042bd7ecea9c7caa607",
+    "url": "/gomoku-react/static/css/main.4db68af1.chunk.css"
+  },
+  {
+    "revision": "4659fcff096334bb3a755cfa07ce5422",
+    "url": "/gomoku-react/index.html"
+  }
+];
